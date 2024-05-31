668c89de56f4f4bb0aef93237eabe8b4ed6d1821
Suman Kumar <Suman.Chava@blueyonder.com>
2024-05-31 23:17:03 +0530

    Added Blazor components project

---
+++ BlazorComponents/Components/CustomInputSelect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Components.Forms;
+
+namespace BlazorComponents.Components
+{
+	public class CustomInputSelect<TValue> : InputSelect<TValue>
+	{
+		
+	}
+}
+
